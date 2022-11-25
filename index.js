f17a3928ed1a4ff276f67278df0a8ecd41d73a00
stellagitonga <stellanyamburagitonga@gmail.com>
2022-11-25 11:27:40 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+let num1 = 1
+let num2 = 62
+let multiply = num1 * num2
 
-// Write your code here
+const random = Math.floor(Math.random() *35 + 16);
+
+let num3 = 4;
+let num4 = 5;
+
+let mod = num3 % num4;
+
+let max = Math.max(10, 20, 15, 8);
+console.log(max);
+
+
+
+
+
+    
+
+
+
+
+
